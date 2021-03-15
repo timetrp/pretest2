a19ede746e09cdb50a344c845bae5e7fa2c23db6
timetrp <terapat.chn@gmail.com>
2021-03-15 16:38:06 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
+const fetchCategories = () => {
+  return fetch("https://api.publicapis.org/categories")
+    .then((resp) => resp.json())
+    .then((json) => json);
+};
 function App() {
+  const [inputVal, setInputVal] = useState("");
+
+  const [categories, setCategories] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetchCategories();
+      setCategories(data);
+    })();
+  }, []);
+  const handleChange = (e) => {
+    setInputVal(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={handleChange} />
+      {categories && (
+        <section>
+          <table>
+            {categories
+              .filter((cat) =>
+                cat.toLowerCase().includes(inputVal.toLowerCase())
+              )
+              .map((cat) => (
+                <tr>
+                  <td>{cat}</td>
+                </tr>
+              ))}
+          </table>
+        </section>
+      )}
     </div>
   );
 }
